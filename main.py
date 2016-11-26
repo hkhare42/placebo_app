1a011a6b2b65cfec48254b1402588d990ff9b968
Harshit <khareharshit@gmail.com>
2016-11-26 21:23:12 +0530

    Add screens to app

--- main.py
+++ main.py
@@ -1,10 +1,15 @@
 from kivy.app import App
+from kivy.uix.screenmanager import ScreenManager
+
+class MultiScreens(ScreenManager):
+    """ Screen management class containing most of app. """
+    pass
 
 class PlaceboApp(App):
     """ Main app class. """
     
     def build(self):
-        pass
+        return MultiScreens()
 
 if __name__ == '__main__':
     PlaceboApp().run()
