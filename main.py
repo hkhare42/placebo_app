8f80c36ae817c9c6b0a8c9d538a75cd8e973fb3d
Harshit <khareharshit@gmail.com>
2016-11-29 11:20:48 +0530

    Add more messages and randomization

--- main.py
+++ main.py
@@ -1,9 +1,12 @@
 from kivy.app import App
 from kivy.uix.screenmanager import ScreenManager
+import codecs
 
 class MultiScreens(ScreenManager):
     """ Screen management class containing most of app. """
-    pass
+    # Picked from http://www.alexandrafranzen.com/2012/02/25/50-ways-to-say-youre-awesome/
+    message_string = [line.strip() for line in codecs.open(
+        'messages.txt', 'r', encoding='utf-8', errors='ignore')]
 
 class PlaceboApp(App):
     """ Main app class. """
