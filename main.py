e044ee791cbbef91b51870d4302c01ad29d71953
Harshit <khareharshit@gmail.com>
2016-12-04 13:28:11 +0530

    Add POST response to server.

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
 from kivy.app import App
 from kivy.uix.screenmanager import ScreenManager
 import codecs
+import requests
 
 class MultiScreens(ScreenManager):
     """ Screen management class containing most of app. """
@@ -8,11 +9,20 @@ class MultiScreens(ScreenManager):
     message_string = [line.strip() for line in codecs.open(
         'messages.txt', 'r', encoding='utf-8', errors='ignore')]
 
+    def send_resp(self, response):
+        ind = 1 if response == 'YES' else 0
+        requests.post('http://127.0.0.1:5000/entry',
+            data= {'user_id': 0, 'msg_id': 0, 'response': ind})
+        self.current = 'home'
+
 class PlaceboApp(App):
     """ Main app class. """
     
     def build(self):
         return MultiScreens()
+
+    def open_settings(*args):
+        pass
 
 if __name__ == '__main__':
     PlaceboApp().run()
